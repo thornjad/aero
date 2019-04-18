c063e1ca288a898e334a0e53b2a9d3bd9c1cde93
Jade Michael Thornton <jade@jmthornton.net>
2019-04-18 17:04:39 -0500

    use aero versions

--- core/core-versions.el
+++ core/core-versions.el
@@ -1,13 +1,10 @@
-;;; core-versions.el --- Spacemacs Core File
-;;
-;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
-;;
-;; Author: Sylvain Benner <sylvain.benner@gmail.com>
-;; URL: https://github.com/syl20bnr/spacemacs
+;;; core-versions.el --- Aero Core File
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
 
+(defconst aero-version                 "2.0.0" "Aero version.")
+(defconst aero-emacs-min-version        "25.1" "Minimal version of Emacs.")
 (defconst spacemacs-version          "0.300.0" "Spacemacs version.")
 (defconst spacemacs-emacs-min-version   "25.1" "Minimal version of Emacs.")
--- init.el
+++ init.el
@@ -1,15 +1,12 @@
-;;; init.el --- Spacemacs Initialization File
+;;; init.el --- Aero Initialization File
 ;;
-;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
-;;
-;; Author: Sylvain Benner <sylvain.benner@gmail.com>
-;; URL: https://github.com/syl20bnr/spacemacs
+;; Copyright (c) 2018-2019 Jade Michael Thornton
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
 
-;; Without this comment emacs25 adds (package-initialize) here
+;; NOTE: without this comment emacs25 adds (package-initialize) here
 ;; (package-initialize)
 
 ;; Avoid garbage collection during startup.
@@ -25,10 +22,10 @@
 (load (concat spacemacs-core-directory "core-dumper.el")
       nil (not init-file-debug))
 
-(if (not (version<= spacemacs-emacs-min-version emacs-version))
+(if (not (version<= aero-emacs-min-version emacs-version))
     (error (concat "Your version of Emacs (%s) is too old. "
-                   "Spacemacs requires Emacs version %s or above.")
-           emacs-version spacemacs-emacs-min-version)
+                   "Aero requires Emacs version %s or above.")
+           emacs-version aero-emacs-min-version)
   ;; Disable file-name-handlers for a speed boost during init
   (let ((file-name-handler-alist nil))
     (require 'core-spacemacs)
