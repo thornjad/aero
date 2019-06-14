ba9bfc659196d4a5521e5e8c674b88a1c04ac0f9
Jade Michael Thornton <jade@jmthornton.net>
2019-06-14 15:41:41 -0500

    more hellish landscape

--- layers/aero-prelude.el
+++ layers/aero-prelude.el
@@ -16,30 +16,60 @@
 ;; garder ma merde à jour
 
 (use-package auto-package-update :ensure t
-	:config
-	(setq auto-package-update-delete-old-versions t
-				auto-package-update-hide-results t)
-	(auto-package-update-maybe))
+  :config
+  (setq auto-package-update-delete-old-versions t
+        auto-package-update-hide-results t)
+  (auto-package-update-maybe))
 
 
 ;; we descend to hell
 
 (use-package evil :ensure t
-	:commands (evil-mode)
-	:init
-	(aero/add-hook!
-	 'after-init-hook
-	 (evil-mode 1)))
+  :commands (evil-mode)
+  :init
+  (aero/add-hook!
+   'after-init-hook
+   (evil-mode 1)
+   (setq evil-want-fin-undo t)))
+
+(use-package evil-matchit :ensure t
+  :commands
+  evilmi-jump-items
+  :config
+  (global-evil-matchit-mode 1))
+
+(use-package evil-surround :ensure t
+  :config
+  (global-evil-surround-mode))
+
+(use-package evil-visualstar :ensure t
+  :config
+  (global-evil-visualstar-mode t))
+
+(setq evil-default-state 'normal)
+(evil-set-initial-state 'dired-mode 'emacs)
+(evil-set-initial-state 'message-mode 'motion)
+(evil-set-initial-state 'help-mode 'emacs)
+(evil-set-initial-state 'ivy-occur-mode 'emacs)
+(evil-set-initial-state 'calendar-mode 'emacs)
+(evil-set-initial-state 'esup-mode 'emacs)
+
+;; cursor color by state
+(setq evil-insert-state-cursor  '("#268bd2" hbar)  ;; blue
+      evil-normal-state-cursor  '("#b58900" box)  ;; blue
+      evil-visual-state-cursor  '("#cb4b16" bar)  ;; orange
+      evil-replace-state-cursor '("#859900" hbar) ;; green
+      evil-emacs-state-cursor   '("#d33682" box)) ;; magenta
 
 
 ;; ido-ido
 
 (use-package counsel :ensure t
-	:config
-	(setq counsel-find-file-ignore-regexp
-				(concat "\\(?:\\`[#.]\\)\\|\\(?:[#~]\\'\\)"
-								"\\|\\.x\\'\\|\\.d\\'\\|\\.o\\'"
-								"\\|\\.aux\\'")))
+  :config
+  (setq counsel-find-file-ignore-regexp
+        (concat "\\(?:\\`[#.]\\)\\|\\(?:[#~]\\'\\)"
+                "\\|\\.x\\'\\|\\.d\\'\\|\\.o\\'"
+                "\\|\\.aux\\'")))
 
 
 ;; Set up global functionality
