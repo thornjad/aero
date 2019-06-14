9b63bdff4eab56e57f72a52ff3bf3d997f45dbb7
Jade Michael Thornton <jade@jmthornton.net>
2019-06-14 17:12:59 -0500

    set up defaults

--- core/aero-rc.el
+++ core/aero-rc.el
@@ -11,56 +11,93 @@
 ;;
 ;; This file is not part of GNU Emacs
 
-(setq
+;; global defaults
+(setq-default
+
+ ;; general
+ initial-scratch-message "Welcome to Aero"
+ ring-bell-function 'ignore ; supprime cette putain de cloche.
+ sentence-end-double-space nil
+ default-fill-column 80
+ help-window-select t       ; focus help window when opened
+ kill-ring-max 5000                     ;truncate kill ring after 5000 entries
+ mark-ring-max 5000                     ;truncate mark ring after 5000 entries
+ mouse-wheel-scroll-amount '(1 ((shift) . 5) ((control))) ;make mouse scrolling smooth
+ apropos-do-all t
+ mouse-yank-at-point t
+
+ ;; version control and saving
  use-package-verbose nil
  delete-old-versions -1	    ; supprime les vieilles versions des fichiers sauvegardés
  backup-directory-alist `(("." . "~/.emacs.d/backups"))
  version-control t
  vc-make-backup-files t	    ; backups file even when under vc
  vc-follow-symlinks t
+ git-commit-fill-column 72
  auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t))
- initial-scratch-message "Welcome to Aero"
- ring-bell-function 'ignore ; supprime cette putain de cloche.
- sentence-end-double-space nil
- default-fill-column 80
- initial-scratch-message ""
  save-interprogram-paste-before-kill t
- help-window-select t       ; focus help window when opened
- tab-width 2                ; onglet affiché sous forme de 2
- auto-window-vscroll nil
- )
-(setq-default
+
+ ;; files
+ confirm-nonexistent-file-or-buffer nil ;don't ask to create a buffer
+ recentf-max-saved-items 5000           ;save up to 5000 recent files
+ require-final-newline t
+ load-prefer-newer t
+ scroll-margin 3
+
+ ;; indentation
  indent-tabs-mode t
- tab-width 2
- c-basic-offset 2)
+ tab-stop-list (number-sequence 2 200 2)
+ tab-width 2                ; onglet affiché sous forme de 2
+ c-basic-offset 2
+ standard-indent 2
+ auto-window-vscroll nil)
+
+;; ensure lang is set properly
 (setenv "LANG" "en_US.UTF-8")
 (setenv "LC_ALL" "en_US.UTF-8")
 
 ;; store all backup and autosave files in the tmp dir
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+(setq-default backup-directory-alist `((".*" . ,temporary-file-directory))
+							auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
 ;; remplace yes no par y n
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; display changes
-(show-paren-mode) ; highlight delimiters
+(show-paren-mode 1) ; highlight delimiters
 (line-number-mode -1) ; only show line number in mode line
 (column-number-mode -1) ; also show column in mode line
-(setq initial-major-mode 'fundamental-mode)
+(setq-default initial-major-mode 'fundamental-mode)
 (aero/add-hook! 'before-save-hook
 	(delete-trailing-whitespace))
+(add-hook 'text-mode-hook 'turn-on-visual-line-mode)
+(add-hook 'fundamental-mode-hook 'turn-on-visual-line-mode)
+
+;; type to get rid of active selection
+(delete-selection-mode t)
 
 ;; rend les scripts executable par défault si c'est un script.
 (aero/add-hook! 'after-save-hook
 	(executable-make-buffer-file-executable-if-script-p))
 
+;; turn off unneeded stuff when unneeded
+(unless (eq window-system 'ns)
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
+
+;; ensure buffer names are unique
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
 (defvar aero/default-font "Dank Mono"
 	"Default font throughout Aero")
 
-(set-face-attribute 'default nil :font aero/default-font)
+(global-prettify-symbols-mode t)
 
 (defvar aero/gc-cons '(100000000 0.1))
 
