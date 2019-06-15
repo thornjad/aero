4a3886ea81cc6222af33b08a723b8c9e3c86618f
Jade Michael Thornton <jade@jmthornton.net>
2019-06-14 22:10:38 -0500

    move file functions to own file
    and add some too

--- core/aero-util.el
+++ core/aero-util.el
@@ -12,6 +12,7 @@
 ;; This file is not part of GNU Emacs
 
 (use-package cl-lib :ensure t)
+(require 'aero-files)
 
 
 ;; loading
@@ -169,69 +170,6 @@ If FUNC is a lambda you must give it a name with FNAME. "
   (interactive)
   (let ((message-log-max nil))
     (apply 'message msg args)))
-
-
-;; files and buffers
-(defun aero/delete-this-file ()
-  "Delete the current file, and kill the buffer."
-  (interactive)
-  (or (buffer-file-name) (error "No file is currently being edited"))
-  (when (yes-or-no-p (format "Really delete '%s'?"
-                             (file-name-nondirectory buffer-file-name)))
-    (delete-file (buffer-file-name))
-    (kill-this-buffer)))
-
-(defun aero/rename-this-file-and-buffer (new-name)
-  "Renames both current buffer and file it's visiting to NEW-NAME."
-  (interactive "sNew name: ")
-  (let ((name (buffer-name))
-        (filename (buffer-file-name)))
-    (unless filename
-      (error "Buffer '%s' is not visiting a file!" name))
-    (if (get-buffer new-name)
-        (message "A buffer named '%s' already exists!" new-name)
-      (progn
-        (rename-file filename new-name 1)
-        (rename-buffer new-name)
-        (set-visited-file-name new-name)
-        (set-buffer-modified-p nil)))))
-
-(defun aero/buffer-too-big-p ()
-	"Is buffer longer than 5000"
-  (> (buffer-size) (* 5000 80)))
-
-(defun aero/file-too-big-p (file)
-	"Is file larger than 5000 lines"
-  (> (nth 7 (file-attributes file))
-     (* 5000 64)))
-
-(defun aero/open-in-external-app ()
-  "Open current file in external application."
-  (interactive)
-  (let ((file-path (if (eq major-mode 'dired-mode)
-                       (dired-get-file-for-visit)
-                     (buffer-file-name))))
-    (if file-path
-        (shell-command (format "open \"%s\"" file-path))
-      (message "No file associated to this buffer."))))
-
-(defun aero/finder-here ()
-	"Open macOS Finder here"
-  (interactive)
-	(when (system-is-mac)
-		(let* ((dir default-directory)
-					 (scr (format " do shell script \"open %s\"\n" dir)))
-			(do-applescript scr))))
-
-;; adapted from
-;; http://emacs.stackexchange.com/questions/202/close-all-dired-buffers
-(defun aero/kill-diff-buffers ()
-  (interactive)
-  (mapc (lambda (buffer)
-          (when (member (buffer-local-value 'major-mode buffer)
-                        '(diff-mode magit-diff-mode magit-process-mode))
-            (kill-buffer buffer)))
-        (buffer-list)))
 
 
 ;; general nice functions
