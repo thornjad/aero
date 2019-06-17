42890c56c1f0a4b509340d14ba0fb7b118ef8960
Jade Michael Thornton <jade@jmthornton.net>
2019-06-17 16:25:21 -0500

    remove useless progn

--- layers/aero-sql.el
+++ layers/aero-sql.el
@@ -13,34 +13,33 @@
 (use-package sql
   :defer t
   :config
-  (progn
-    (defun aero/sql-send-string-and-focus ()
-      "Send a string to SQLi and switch to SQLi in `insert state'."
-      (interactive)
-      (let ((sql-pop-to-buffer-after-send-region t))
-        (call-interactively 'sql-send-string)
-        (evil-insert-state)))
+  (defun aero/sql-send-string-and-focus ()
+    "Send a string to SQLi and switch to SQLi in `insert state'."
+    (interactive)
+    (let ((sql-pop-to-buffer-after-send-region t))
+      (call-interactively 'sql-send-string)
+      (evil-insert-state)))
 
-    (defun aero/sql-send-buffer-and-focus ()
-      "Send the buffer to SQLi and switch to SQLi in `insert state'."
-      (interactive)
-      (let ((sql-pop-to-buffer-after-send-region t))
-        (sql-send-buffer)
-        (evil-insert-state)))
+  (defun aero/sql-send-buffer-and-focus ()
+    "Send the buffer to SQLi and switch to SQLi in `insert state'."
+    (interactive)
+    (let ((sql-pop-to-buffer-after-send-region t))
+      (sql-send-buffer)
+      (evil-insert-state)))
 
-    (defun aero/sql-send-paragraph-and-focus ()
-      "Send the paragraph to SQLi and switch to SQLi in `insert state'."
-      (interactive)
-      (let ((sql-pop-to-buffer-after-send-region t))
-        (sql-send-paragraph)
-        (evil-insert-state)))
+  (defun aero/sql-send-paragraph-and-focus ()
+    "Send the paragraph to SQLi and switch to SQLi in `insert state'."
+    (interactive)
+    (let ((sql-pop-to-buffer-after-send-region t))
+      (sql-send-paragraph)
+      (evil-insert-state)))
 
-    (defun aero/sql-send-region-and-focus (start end)
-      "Send region to SQLi and switch to SQLi in `insert state'."
-      (interactive "r")
-      (let ((sql-pop-to-buffer-after-send-region t))
-        (sql-send-region start end)
-        (evil-insert-state)))))
+  (defun aero/sql-send-region-and-focus (start end)
+    "Send region to SQLi and switch to SQLi in `insert state'."
+    (interactive "r")
+    (let ((sql-pop-to-buffer-after-send-region t))
+      (sql-send-region start end)
+      (evil-insert-state))))
 
 (use-package sql-indent :defer t)
 
