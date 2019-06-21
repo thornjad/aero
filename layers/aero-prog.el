81eb957bedfe9bac07db3716e399e5b9aca7658b
Jade Michael Thornton <jade@jmthornton.net>
2019-06-21 17:16:12 -0500

    smartparens and gtags

--- layers/aero-prog.el
+++ layers/aero-prog.el
@@ -23,4 +23,34 @@
         company-show-numbers t
 				company-tooltip-align-annotations t))
 
+;; TODO bindings
+(use-package counsel-gtags :ensure t)
+
+(use-package smartparens :ensure t
+  :commands smartparens-global-mode
+  :hook ((after-init . smartparens-global-mode)
+         (prog-mode . smartparens-strict-mode))
+  :config
+  (require 'smartparens-config)
+  (show-smartparens-global-mode t)
+
+  (general-define-key
+   :states 'normal
+   :prefix "SPC"
+   "s0" '(sp-beginning-of-sexp :which-key "beginning of sexp")
+   "s$" '(sp-end-of-sexp :which-key "end of sexp")
+   "sk" '(sp-up-sexp :which-key "up sexp")
+   "sj" '(sp-down-sexp :which-key "down sexp")
+   "sh" '(sp-backward-sexp :which-key "back sexp")
+   "sl" '(sp-forward-sexp :which-key "forward sexp")
+   "su" '(sp-unwrap-sexp :which-key "unwrap sexp")
+   "sK" '(sp-kill-sexp :which-key "kill sexp"))
+
+  (sp-local-pair 'web-mode "<? " " ?>")
+  (sp-local-pair 'web-mode "{ " " }")
+  (sp-local-pair 'web-mode "{{ "  " }}")
+  (sp-local-pair 'org-mode "$" "$")
+  (sp-local-pair 'org-mode "=" "=")
+  (sp-local-pair 'org-mode "/" "/" :trigger-wrap "/" ))
+
 (provide 'aero-prog)
