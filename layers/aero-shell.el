b4e7372985dee0921326cdb35057250c224c1543
Jade Michael Thornton <jade@jmthornton.net>
2019-06-22 10:47:12 -0500

    shell mode

--- layers/aero-shell.el
+++ layers/aero-shell.el
@@ -29,4 +29,40 @@
     "\n"
     (propertize " └─ λ " 'face '(:foreground "green")))))
 
+(use-package sh-script :defer t
+  :mode
+  (("\\.sh\\'" . shell-script-mode)
+   ("\\.bash\\'" . shell-script-mode)
+   ("\\.zsh\\'" . shell-script-mode)
+   ("zlogin\\'" . shell-script-mode)
+   ("zlogout\\'" . shell-script-mode)
+   ("zpreztorc\\'" . shell-script-mode)
+   ("zprofile\\'" . shell-script-mode)
+   ("zshenv\\'" . shell-script-mode)
+   ("zshrc\\'" . shell-script-mode))
+  :config
+  (setq shell-file-name "/usr/local/bin/bash")
+
+  (use-package company-shell
+    :ensure t
+    :config
+    (add-hook!
+     'sh-mode-hook
+     (set-local-company-backends! 'company-shell)))
+
+  (defun indent-paragraph ()
+    (interactive)
+    (save-excursion
+      (mark-paragraph) (indent-region (region-beginning) (region-end))))
+
+  (defun sh-cleanup-line ()
+    (interactive)
+    (let* ((beg (line-beginning-position)))
+      (save-excursion
+        (end-of-line)
+        (while (re-search-backward "--\\||\\|([><])\{1,2\}" beg t)
+          (insert "\\")
+          (newline-and-indent))
+        (indent-paragraph))))
+
 (provide 'aero-shell)
