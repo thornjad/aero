cb7faec9afbc23da46a2741b277a425291990d78
Jade Michael Thornton <jade@jmthornton.net>
2019-07-08 11:32:05 -0500

    clean up shell a bit

--- layers/aero-shell.el
+++ layers/aero-shell.el
@@ -27,72 +27,7 @@
       (propertize " ┌─── " 'face '(:foreground "green"))
       (propertize (eshell/pwd) 'face '(:weight ultra-bold))
       "\n"
-      (propertize " └─ λ " 'face '(:foreground "green")))))
-
-  ;; from aweshell by Andy Stewart
-  (defun aero/validate-command ()
-    "Validate command and colorize before send to eshell."
-    (save-excursion
-      (beginning-of-line)
-      (re-search-forward (format "%s\\([^ \t\r\n\v\f]*\\)" eshell-prompt-regexp)
-                         (line-end-position)
-                         t)
-      (let ((beg (match-beginning 1))
-            (end (match-end 1))
-            (command (match-string 1)))
-        (when command
-          (put-text-property
-           beg end
-           'face `(:foreground
-                   ,(if
-                        (or
-                         ;; Command exists?
-                         (executable-find command)
-                         ;; Or command is an alias?
-                         (seq-contains (eshell-alias-completions "") command)
-                         ;; Or it is ../. ?
-                         (or (equal command "..")
-                             (equal command ".")
-                             (equal command "exit"))
-                         ;; Or it is a file in current dir?
-                         (member (file-name-base command) (directory-files default-directory))
-                         ;; Or it is a elisp function
-                         (functionp (intern command)))
-                        "#98C379"
-                      "#FF0000")))
-          (put-text-property beg end 'rear-nonsticky t)))))
-
-  (add-hook 'eshell-mode-hook
-            (lambda ()
-              (add-hook 'post-command-hook #'aero/validate-command t t)))
-
-  (defun aero/cat-with-syntax-highlight (filename)
-    "Like cat(1) but with syntax highlighting."
-    (let ((existing-buffer (get-file-buffer filename))
-          (buffer (find-file-noselect filename)))
-      (eshell-print
-       (with-current-buffer buffer
-         (if (fboundp 'font-lock-ensure)
-             (font-lock-ensure)
-           (with-no-warnings
-             (font-lock-fontify-buffer)))
-         (buffer-string)))
-      (unless existing-buffer
-        (kill-buffer buffer))
-      nil))
-
-  (advice-add 'eshell/cat :override #'aero/cat-with-syntax-highlight)
-
-  (use-package eshell-did-you-mean
-    :load-path aero-packages-directory
-    :config
-    (add-hook 'eshell-mode-hook
-              (lambda ()
-                (run-with-idle-timer
-                 1 nil
-                 #'(lambda ()
-                     (eshell-did-you-mean-setup)
-                     ))))))
+      (propertize " └─ λ " 'face '(:foreground "green"))))))
 
 ;;; shell scripting
 
@@ -131,7 +66,5 @@
           (insert "\\")
           (newline-and-indent))
         (indent-paragraph)))))
-
-(use-package xterm-color :ensure t)
 
 (provide 'aero-shell)
