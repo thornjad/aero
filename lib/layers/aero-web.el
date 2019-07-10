3bd97b98a948887b1cba0d6af58bab3096436241
Jade Michael Thornton <jade@jmthornton.net>
2019-07-10 12:51:18 -0500

    remove apache mode
    conf mode is fine

--- lib/layers/aero-web.el
+++ lib/layers/aero-web.el
@@ -97,9 +97,4 @@
   ((coffee . "npm i -g coffeescript")
    (coffeelint . "npm i -g coffeelint")))
 
-;; major mode for editing Apache configuration files
-(use-package apache-mode :ensure t
-  :defer t
-  :quelpa (apache-mode :fetcher github :repo "emacsmirror/apache-mode"))
-
 (provide 'aero-web)
