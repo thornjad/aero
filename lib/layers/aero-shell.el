1fb52c9d7616d37747b6d75633ba56804a2cef33
Jade Michael Thornton <jade@jmthornton.net>
2019-07-11 15:36:35 -0500

    eshell improvements

--- lib/layers/aero-shell.el
+++ lib/layers/aero-shell.el
@@ -17,9 +17,37 @@
   :commands eshell
   :config
   (setq
-   eshell-aliases-file (concat user-emacs-directory "eshell-aliases")
    eshell-save-history-on-exit t
-   eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.git\\)/\\'"))
+   eshell-buffer-maximum-lines 12000
+   eshell-history-size 500
+   eshell-ls-initial-args "-lah"
+   eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.git\\)/\\'"
+   eshell-visual-commands '("vi" "screen" "top" "less" "more" "lynx"
+                            "ncftp" "pine" "tin" "trn" "elm" "vim"
+                            "nmtui" "alsamixer" "htop" "el" "elinks"
+                            "ssh" "nethack" "dtop" "dstat")
+   eshell-visual-subcommands '(("git" "log" "diff" "show")))
+
+  ;; doesn't handle less too well
+  (setenv "PAGER" "cat")
+
+  (defun eshell/cds ()
+    "Change directory to git project root."
+    (eshell/cd (locate-dominating-file default-directory ".git")))
+  (defalias 'eshell/la 'eshell/ls)
+  (defun eshell/ec (pattern)
+    (if (stringp pattern)
+        (find-file pattern)
+      (mapc #'find-file (mapcar #'expand-file-name pattern))))
+  (defalias 'e 'eshell/ec)
+  (defalias 'ee 'find-file-other-window)
+  (defun eshell/clear ()
+    (recenter 0))
+  (defun eshell/magit ()
+    "Open magit-status in the current directory."
+    (interactive)
+    (magit-status default-directory))
+  )
 
 ;;; shell scripting
 
