98efce3a17d924f35aa95f429aad5e6dfa935fc9
Jade Michael Thornton <jade@jmthornton.net>
2019-07-11 08:20:18 -0500

    add some evil eww bindings

--- lib/layers/aero-eww.el
+++ lib/layers/aero-eww.el
@@ -10,13 +10,23 @@
 ;;
 ;; This file is not part of GNU Emacs
 
+(require 'evil)
+
 (setq browse-url-browser-function #'eww-browse-url)
 (use-package eww
   :commands (eww
              eww-browse-url
              eww-search-words)
+  :init
+  (general-define-key
+   :states '(normal visual)
+   :prefix "SPC"
+    "ws" '(eww-search-words :which-key "web search")
+    "ww" 'eww)
+
 	:config
 	(setq eww-search-prefix "https://duckduckgo.com/lite?q=")
+
 	(defun add-title-to-eww-buffer-name ()
 		"Rename eww mode buffer so the title of the page is displayed, making
 		 fake-tabbed-browsing easier"
@@ -26,9 +36,49 @@
 						(rename-buffer (concat "eww - " title) t)
 					(rename-buffer "eww" t)))))
 	(add-hook 'eww-after-render-hook 'add-title-to-eww-buffer-name)
-  (general-define-key
-   :states '(normal visual)
-   :prefix "SPC"
-   "ws" '(eww-search-words :which-key "web search")))
+
+  ;; normal browsing
+  (evil-define-key 'normal 'eww-mode-map
+    "^" 'eww-up-url
+    "u" 'eww-up-url
+    "U" 'eww-top-url
+    (kbd "<backspace>") 'eww-back-url
+    "H" 'eww-back-url
+    "L" 'eww-forward-url
+    "&" 'eww-browse-with-external-browser
+    "d" 'eww-download
+    "m" 'eww-add-bookmark
+    "R" 'eww-readable
+    "r" 'eww-reload
+    "gr" 'eww-reload
+    "q" 'kill-buffer
+    "Q" 'quit-window
+    "o" 'eww
+    "gf" 'eww-view-source
+    "gc" 'url-cookie-list
+    "gh" 'eww-list-histories
+    "gb" 'eww-list-buffers
+    "gt" 'eww-list-buffers)
+
+  ;; viewing history
+  (evil-set-initial-state 'normal 'eww-history-mode)
+  (evil-define-key 'normal 'eww-history-mode-map
+    (kbd "RET") 'eww-history-browse
+    "q" 'quit-window)
+
+  ;; viewing buffers
+  (evil-set-initial-state 'eww-buffers-mode 'normal)
+  (evil-define-key 'normal 'eww-buffers-mode-map
+    "D" 'eww-buffer-kill
+    (kbd "RET") 'eww-buffer-select
+    "q" 'quit-window)
+
+  ;; bookmarks
+  (evil-set-initial-state 'eww-bookmark-mode 'normal)
+  (evil-define-key 'normal 'eww-bookmark-mode-map
+    "D" 'eww-bookmark-kill
+    "P" 'eww-bookmark-yank
+    (kbd "RET") 'eww-bookmark-browse
+    "q" 'quit-window))
 
 (provide 'aero-eww)
