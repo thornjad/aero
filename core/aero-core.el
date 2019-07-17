1871564dfe4051c971f4bd81de6fd766e44e59f1
Jade Michael Thornton <jade@jmthornton.net>
2019-07-17 15:34:08 -0500

    untabify core

--- core/aero-core.el
+++ core/aero-core.el
@@ -14,26 +14,26 @@
 ;;; core functionality and utils
 
 (defun aero/bootstrap ()
-	"Bootstrap `use-package', `quelpa' and major fixes, and set up for use"
+  "Bootstrap `use-package', `quelpa' and major fixes, and set up for use"
 
-	(setq package-enable-at-startup nil)
-	(let ((default-directory "~/.emacs.d/elpa"))
-		(unless (file-directory-p default-directory)
-			(make-directory default-directory))
-		(normal-top-level-add-subdirs-to-load-path))
+  (setq package-enable-at-startup nil)
+  (let ((default-directory "~/.emacs.d/elpa"))
+    (unless (file-directory-p default-directory)
+      (make-directory default-directory))
+    (normal-top-level-add-subdirs-to-load-path))
 
   ;; (require 'package)
-	(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-													 ("melpa" . "https://melpa.org/packages/")
-													 ("melpa-stable" . "https://stable.melpa.org/packages/")
-													 ("org" . "https://orgmode.org/elpa/")))
-	(package-initialize t)
-	(unless (package-installed-p 'use-package)
-		(package-refresh-contents)
-		(package-install 'use-package))
-	(eval-when-compile
-		(require 'use-package)
-		(setq use-package-expand-minimally byte-compile-current-file
+  (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                           ("melpa" . "https://melpa.org/packages/")
+                           ("melpa-stable" . "https://stable.melpa.org/packages/")
+                           ("org" . "https://orgmode.org/elpa/")))
+  (package-initialize t)
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+  (eval-when-compile
+    (require 'use-package)
+    (setq use-package-expand-minimally byte-compile-current-file
           use-package-verbose init-file-debug))
 
   (use-package use-package-ensure-system-package :ensure t)
@@ -44,26 +44,26 @@
 
 
 (defgroup aero nil
-	"Aero customizations"
-	:group 'starter-kit
-	:prefix 'aero/)
+  "Aero customizations"
+  :group 'starter-kit
+  :prefix 'aero/)
 
 
 ;;; init functions
 
 (defun aero/load-libs ()
-	"Load Aero libraries and utilities, which will be required later"
+  "Load Aero libraries and utilities, which will be required later"
 
-	(require 'subr-x)
-	(require 'aero-util)
-	(require 'aero-lib))
+  (require 'subr-x)
+  (require 'aero-util)
+  (require 'aero-lib))
 
 (defun aero/init ()
-	"Perform startup initialization, including all comilation and loading"
-	(aero/bootstrap)
-	(aero/load-libs)
-	(require 'aero-layers)
-	(aero/load-layers)
+  "Perform startup initialization, including all comilation and loading"
+  (aero/bootstrap)
+  (aero/load-libs)
+  (require 'aero-layers)
+  (aero/load-layers)
   (aero/load-private)
 
   ;; baise cette merde
