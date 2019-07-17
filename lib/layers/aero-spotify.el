38fa472cccd1fabc9e3ca4034b68243c009d9d69
Jade Michael Thornton <jade@jmthornton.net>
2019-07-17 09:45:12 -0500

    add spotify layer

---
+++ lib/layers/aero-spotify.el
@@ -0,0 +1,30 @@
+;; -*- lexical-binding: t -*-
+;;
+;; Copyright (c) 2019 Jade Michael Thornton
+;;
+;; This program is free software; you may redistribute it and/or modify it under
+;; the terms of the GNU General Public License version 3, as published by the
+;; Free Software Foundation. This program carries no warranty whatsoever,
+;; without even the implied warranty of merchantability or fitness for a
+;; particular purpose. See </license> for more details.
+;;
+;; This file is not part of GNU Emacs
+
+(use-package counsel-spotify
+  :load-path "lib/packages/counsel-spotify"
+  :after counsel
+  :commands counsel-spotify-play
+  :config
+  (require 'aero-private-hidden) ; for spotify client id
+  (general-define-key
+   :states 'normal
+   :prefix "SPC"
+    "as" '(:ignore t :which-key "spotify")
+    "asp" '(counsel-spotify-toggle-play-pause :which-key "play/pause")
+    "asn" '(counsel-spotify-next :which-key "next")
+    "asP" '(counsel-spotify-previous :which-key "previous")
+    "ass" 'counsel-spotify-search-track
+    "asa" 'counsel-spotify-search-artist
+    "asA" 'counsel-spotify-search-album))
+
+(provide 'aero-spotify)
