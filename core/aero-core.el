4c138fa2e989e620c92068e34172372e0888b10b
Jade Michael Thornton <jade@jmthornton.net>
2019-10-28 08:36:37 -0500

    dont load package twice

--- core/aero-core.el
+++ core/aero-core.el
@@ -28,7 +28,6 @@
       (make-directory default-directory))
     (normal-top-level-add-subdirs-to-load-path))
 
-  (require 'package)
   (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                            ("melpa" . "https://melpa.org/packages/")
                            ("melpa-stable" . "https://stable.melpa.org/packages/")
