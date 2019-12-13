14c3c45d24db13820c020f7e079299708bf936b6
Jade Michael Thornton <jade@jmthornton.net>
2019-12-13 09:34:31 -0600

    add sort-words function

--- core/aero-lib.el
+++ core/aero-lib.el
@@ -89,6 +89,17 @@ passed, the return value is just PATH."
     (setq path (expand-file-name (pop segments) path)))
   path)
 
+(defun aero/sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+Prefixed with negative \\[universal-argument], sorts in reverse.
+
+The variable `sort-fold-case' determines whether alphabetic case affects the
+sort order.
+
+See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
+
 
 ;;; system and logging
 
