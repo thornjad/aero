ecb1fd4dd42f38f96c6908aac5088850b1b4e549
Jade Michael Thornton <jade@jmthornton.net>
2019-12-31 14:37:57 -0600

    internalize naming convention

--- lib/layers/aero-theme.el
+++ lib/layers/aero-theme.el
@@ -119,11 +119,11 @@
   (match (:foreground aero-faded-black :background aero-faded-blue))
 
   ;; Aero modeline
-  (aero-modeline-evil-normal (:foreground aero-bg :background aero-faded-cyan))
-  (aero-modeline-evil-insert (:foreground aero-bg :background aero-faded-magenta))
-  (aero-modeline-evil-visual (:foreground aero-bg :background aero-faded-green))
-  (aero-modeline-evil-replace (:foreground aero-bg :background aero-bright-orange))
-  (aero-modeline-evil-emacs (:foreground aero-bg :background aero-bright-red))
+  (aero/modeline-evil-normal (:foreground aero-bg :background aero-faded-cyan))
+  (aero/modeline-evil-insert (:foreground aero-bg :background aero-faded-magenta))
+  (aero/modeline-evil-visual (:foreground aero-bg :background aero-faded-green))
+  (aero/modeline-evil-replace (:foreground aero-bg :background aero-bright-orange))
+  (aero/modeline-evil-emacs (:foreground aero-bg :background aero-bright-red))
 
   ;; Customize faces
   (widget-field (:background aero-grey1))
@@ -557,75 +557,75 @@
      ,aero-faded-yellow])
   `(pdf-view-midnight-colors '(,aero-fg . ,aero-bg))))
 
-(defvar aero-modeline--current-window)
+(defvar aero/modeline--current-window)
 (defvar flycheck-current-errors)
 (declare-function flycheck-count-errors "flycheck" (errors))
 
 ;;; Config
 
-(defgroup aero-modeline nil
+(defgroup aero/modeline nil
   "A minimal mode-line configuration inspired by doom-modeline."
   :group 'mode-line)
 
-(defface aero-modeline-status-grayed-out
+(defface aero/modeline-status-grayed-out
   '((t (:inherit (font-lock-doc-face) :slant italic)))
   "Face used for neutral or inactive status indicators in the mode-line."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-status-info
+(defface aero/modeline-status-info
   '((t (:inherit (font-lock-keyword-face) :slant italic)))
   "Face used for generic status indicators in the mode-line."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-status-success
+(defface aero/modeline-status-success
   '((t (:inherit (success) :slant italic)))
   "Face used for success status indicators in the mode-line."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-status-warning
+(defface aero/modeline-status-warning
   '((t (:inherit (warning) :slant italic)))
   "Face for warning status indicators in the mode-line."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-status-error
+(defface aero/modeline-status-error
   '((t (:inherit (error) :slant italic)))
   "Face for error stauts indicators in the mode-line."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-unimportant
+(defface aero/modeline-unimportant
   '((t (:inherit (font-lock-doc-face))))
   "Face used for less important mode-line elements."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-modified
+(defface aero/modeline-modified
   '((t (:inherit (error))))
   "Face used for the 'modified' indicator symbol in the mode-line."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-evil-normal
+(defface aero/modeline-evil-normal
   '((t (:inherit (font-lock-keyword-face))))
   "Face used for Normal Evil state message."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-evil-insert
+(defface aero/modeline-evil-insert
   '((t (:inherit (font-lock-keyword-face))))
   "Face used for Insert Evil state message."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-evil-visual
+(defface aero/modeline-evil-visual
   '((t (:inherit (font-lock-keyword-face))))
   "Face used for Visual Evil state message."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-evil-replace
+(defface aero/modeline-evil-replace
   '((t (:inherit (font-lock-keyword-face))))
   "Face used for Replace Evil state message."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
-(defface aero-modeline-evil-emacs
+(defface aero/modeline-evil-emacs
   '((t (:inherit (font-lock-keyword-face))))
   "Face used for Emacs Evil state message."
-  :group 'aero-modeline)
+  :group 'aero/modeline)
 
 ;;; Helper functions
 
@@ -640,26 +640,26 @@
      right)))
 
 ;; Define a helper function to determine whether or not the current window is active.
-(defsubst aero-modeline-is-active ()
+(defsubst aero/modeline-is-active ()
   "Return \"t\" if the current window is active, \"nil\" if it is not."
-  (eq (selected-window) aero-modeline--current-window))
+  (eq (selected-window) aero/modeline--current-window))
 
 ;;; Update functions
 
 ;; Window update function
-(defvar-local aero-modeline--current-window (frame-selected-window))
-(defun aero-modeline--update-selected-window (&rest _)
-  "Update the `aero-modeline--current-window' variable."
+(defvar-local aero/modeline--current-window (frame-selected-window))
+(defun aero/modeline--update-selected-window (&rest _)
+  "Update the `aero/modeline--current-window' variable."
   (when (frame-selected-window)
     (let ((win (frame-selected-window)))
       (unless (minibuffer-window-active-p win)
-        (setq aero-modeline--current-window win)))))
+        (setq aero/modeline--current-window win)))))
 
 ;; Flycheck update function
-(defvar-local aero-modeline--flycheck-text nil)
-(defun aero-modeline--update-flycheck-segment (&optional status)
-  "Update `aero-modeline--flycheck-text' against the reported flycheck STATUS."
-  (setq aero-modeline--flycheck-text
+(defvar-local aero/modeline--flycheck-text nil)
+(defun aero/modeline--update-flycheck-segment (&optional status)
+  "Update `aero/modeline--flycheck-text' against the reported flycheck STATUS."
+  (setq aero/modeline--flycheck-text
         (pcase status
           ('finished (if flycheck-current-errors
                          (let-alist (flycheck-count-errors flycheck-current-errors)
@@ -668,17 +668,17 @@
                                                  (number-to-string sum)
                                                  " Issues ")
                                          'face (if .error
-                                                   'aero-modeline-status-error
-                                                 'aero-modeline-status-warning))))
-                       (propertize "✔  " 'face 'aero-modeline-status-success)))
-          ('running (propertize "Checking  " 'face 'aero-modeline-status-info))
+                                                   'aero/modeline-status-error
+                                                 'aero/modeline-status-warning))))
+                       (propertize "✔  " 'face 'aero/modeline-status-success)))
+          ('running (propertize "Checking  " 'face 'aero/modeline-status-info))
           ('no-checker "")
-          ('errored (propertize "✘  " 'face 'aero-modeline-status-error))
-          ('interrupted (propertize "⏸  " 'face 'aero-modeline-status-grayed-out)))))
+          ('errored (propertize "✘  " 'face 'aero/modeline-status-error))
+          ('interrupted (propertize "⏸  " 'face 'aero/modeline-status-grayed-out)))))
 
 ;;; Segments
 
-(defun aero-modeline-segment-evil-state ()
+(defun aero/modeline-segment-evil-state ()
   "Display current evil state. Requires function `evil-mode'."
   (when (require 'evil nil 'noerror)
     (declare-function evil-state-property "evil")
@@ -686,14 +686,14 @@
     (let* ((state (string-trim (evil-state-property evil-state :tag t)))
            (formed-state (format " %s " state)))
       (cond
-       ((string= state "<N>") (propertize formed-state 'face 'aero-modeline-evil-normal))
-       ((string= state "<I>") (propertize formed-state 'face 'aero-modeline-evil-insert))
-       ((string= state "<V>") (propertize formed-state 'face 'aero-modeline-evil-visual))
-       ((string= state "<R>") (propertize formed-state 'face 'aero-modeline-evil-replace))
-       ((string= state "<E>") (propertize formed-state 'face 'aero-modeline-evil-emacs))
+       ((string= state "<N>") (propertize formed-state 'face 'aero/modeline-evil-normal))
+       ((string= state "<I>") (propertize formed-state 'face 'aero/modeline-evil-insert))
+       ((string= state "<V>") (propertize formed-state 'face 'aero/modeline-evil-visual))
+       ((string= state "<R>") (propertize formed-state 'face 'aero/modeline-evil-replace))
+       ((string= state "<E>") (propertize formed-state 'face 'aero/modeline-evil-emacs))
        (t formed-state)))))
 
-(defun aero-modeline-segment-modified ()
+(defun aero/modeline-segment-modified ()
   "Displays a color-coded buffer modification indicator in the mode-line."
   (propertize
    (if (and
@@ -701,20 +701,20 @@
         (not (string-match-p "\\*.*\\*" (buffer-name))))
        " ✧ "
      "   ")
-   'face 'aero-modeline-modified))
+   'face 'aero/modeline-modified))
 
-(defun aero-modeline-segment-buffer-name-and-size ()
+(defun aero/modeline-segment-buffer-name-and-size ()
   "Displays the name and size of the current buffer in the mode-line."
   (concat (propertize "%b (%I)" 'face 'mode-line-buffer-id) "  "))
 
-(defun aero-modeline-segment-position ()
+(defun aero/modeline-segment-position ()
   "Displays the current cursor position in the mode-line."
   (concat "%l:%c"
           " "
           (propertize "%p%%"
                       'face
-                      (if (aero-modeline-is-active)
-                          'aero-modeline-unimportant
+                      (if (aero/modeline-is-active)
+                          'aero/modeline-unimportant
                         'mode-line-inactive))
           (when (use-region-p)
             (concat
@@ -722,23 +722,23 @@
              ":" (number-to-string (abs (- (point) (mark))))))
           "  "))
 
-(defun aero-modeline-segment-flycheck ()
+(defun aero/modeline-segment-flycheck ()
   "Displays color-coded flycheck information in the mode-line (if available)."
-  aero-modeline--flycheck-text)
+  aero/modeline--flycheck-text)
 
-(defun aero-modeline-segment-process ()
+(defun aero/modeline-segment-process ()
   "Displays the current value of `mode-line-process' in the mode-line."
   (when mode-line-process
     (list mode-line-process "  ")))
 
-(defun aero-modeline-segment-major-mode ()
+(defun aero/modeline-segment-major-mode ()
   "Displays the current major mode in the mode-line."
   (propertize " %m  "
-              'face (if (aero-modeline-is-active)
+              'face (if (aero/modeline-is-active)
                         'bold
-                      'aero-modeline-status-grayed-out)))
+                      'aero/modeline-status-grayed-out)))
 
-(defun aero-modeline-segment-window-number ()
+(defun aero/modeline-segment-window-number ()
   "Displays the current window number as provided by `winum'."
   (when (require 'winum nil 'noerror)
     (declare-function winum-get-number "winum")
@@ -747,23 +747,23 @@
 ;;; Activation function
 
 ;; Store the default mode-line format
-(defvar aero-modeline--default-mode-line mode-line-format)
+(defvar aero/modeline--default-mode-line mode-line-format)
 
-(define-minor-mode aero-modeline-mode
-  "Toggle aero-modeline on or off."
-  :group 'aero-modeline
+(define-minor-mode aero/modeline-mode
+  "Toggle aero/modeline on or off."
+  :group 'aero/modeline
   :global t
   :lighter nil
   (progn
     ;; Setup flycheck hooks
-    (add-hook 'flycheck-status-changed-functions #'aero-modeline--update-flycheck-segment)
-    (add-hook 'flycheck-mode-hook #'aero-modeline--update-flycheck-segment)
+    (add-hook 'flycheck-status-changed-functions #'aero/modeline--update-flycheck-segment)
+    (add-hook 'flycheck-mode-hook #'aero/modeline--update-flycheck-segment)
 
     ;; Setup window update hooks
-    (add-hook 'window-configuration-change-hook #'aero-modeline--update-selected-window)
-    (add-hook 'focus-in-hook #'aero-modeline--update-selected-window)
-    (advice-add #'handle-switch-frame :after #'aero-modeline--update-selected-window)
-    (advice-add #'select-window :after #'aero-modeline--update-selected-window)
+    (add-hook 'window-configuration-change-hook #'aero/modeline--update-selected-window)
+    (add-hook 'focus-in-hook #'aero/modeline--update-selected-window)
+    (advice-add #'handle-switch-frame :after #'aero/modeline--update-selected-window)
+    (advice-add #'select-window :after #'aero/modeline--update-selected-window)
 
     ;; Set the new mode-line-format
     (setq-default mode-line-format
@@ -771,17 +771,17 @@
                      (aero-info-line-format
                       ;; Left
                       (format-mode-line
-                       '((:eval (aero-modeline-segment-evil-state))
-                         (:eval (aero-modeline-segment-modified))
-                         (:eval (aero-modeline-segment-buffer-name-and-size))
-                         (:eval (aero-modeline-segment-position))))
+                       '((:eval (aero/modeline-segment-evil-state))
+                         (:eval (aero/modeline-segment-modified))
+                         (:eval (aero/modeline-segment-buffer-name-and-size))
+                         (:eval (aero/modeline-segment-position))))
 
                       ;; Right
                       (format-mode-line
-                       '((:eval (aero-modeline-segment-flycheck))
-                         (:eval (aero-modeline-segment-process))
-                         (:eval (aero-modeline-segment-major-mode))
-                         (:eval (aero-modeline-segment-window-number))
+                       '((:eval (aero/modeline-segment-flycheck))
+                         (:eval (aero/modeline-segment-process))
+                         (:eval (aero/modeline-segment-major-mode))
+                         (:eval (aero/modeline-segment-window-number))
                          " "))))))))
 
 
