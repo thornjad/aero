86d671c7f8dff4d908c7304be67c44b7f6b9f271
Jade Michael Thornton <jade@jmthornton.net>
2020-02-11 10:57:49 -0600

    binding for revert buffer

--- lib/aero-prelude.el
+++ lib/aero-prelude.el
@@ -609,6 +609,7 @@ just been killed (which happens during Emacs shutdown)."
  "bl" 'ibuffer
  "bL" 'list-buffers
  "bm" 'switch-to-messages-buffer
+ "br" 'revert-buffer
  "bs" 'switch-to-scratch-buffer
  "bd" 'kill-this-buffer
  "bx" 'kill-buffer-and-window
