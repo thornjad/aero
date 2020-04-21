48f2ca898998ffa758ccdc8c2f7e6ceed0277f3f
Jade Michael Thornton <jademichael@jmthornton.net>
2020-04-21 13:11:09 -0500

    load cargo on minor mode

--- lib/layers/aero-rust.el
+++ lib/layers/aero-rust.el
@@ -1,14 +1,20 @@
 ;; -*- lexical-binding: t -*-
 ;;
-;; Copyright (c) 2018-2019 Jade Michael Thornton
-;;
-;; This program is free software; you may redistribute it and/or modify it under
-;; the terms of the GNU General Public License version 3, as published by the
-;; Free Software Foundation. This program carries no warranty whatsoever,
-;; without even the implied warranty of merchantability or fitness for a
-;; particular purpose. See </license> for more details.
+;; Copyright (c) 2018-2020 Jade Michael Thornton
 ;;
 ;; This file is not part of GNU Emacs
+;;
+;; Permission to use, copy, modify, and/or distribute this software for any
+;; purpose with or without fee is hereby granted, provided that the above
+;; copyright notice and this permission notice appear in all copies.
+;;
+;; The software is provided "as is" and the author disclaims all warranties with
+;; regard to this software including all implied warranties of merchantability
+;; and fitness. In no event shall the author be liable for any special, direct,
+;; indirect, or consequential damages or any damages whatsoever resulting from
+;; loss of use, data or profits, whether in an action of contract, negligence or
+;; other tortious action, arising out of or in connection with the use or
+;; performance of this software.
 
 (setenv "PATH" (concat "~/.cargo/bin:" (getenv "PATH")))
 
@@ -48,6 +54,7 @@
   :hook (flycheck-mode . flycheck-rust-setup))
 
 (use-package cargo :straight t
+  :commands cargo-minor-mode
   :hook (rust-mode . cargo-minor-mode))
 
 (use-package toml-mode :straight t
