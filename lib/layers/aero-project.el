01057a7d47a23cdf973f8abdf60c8c4b7bed5121
Jade Michael Thornton <jademichael@jmthornton.net>
2020-10-21 16:29:24 -0500

    use rg in project find

--- lib/layers/aero-project.el
+++ lib/layers/aero-project.el
@@ -18,6 +18,35 @@
 
 (require 'aero-prelude)
 
+;; Use `rg' instead of the default `find'
+(el-patch-feature project)
+(el-patch-defun project--files-in-directory (dir ignores &optional files)
+  (el-patch-remove
+    (require 'find-dired)
+    (require 'xref)
+    (defvar find-name-arg))
+  (let* ((default-directory dir)
+         (localdir (file-local-name (expand-file-name dir)))
+         (command (el-patch-swap
+                    (format "%s %s %s -type f %s -print0"
+                            find-program
+                            localdir
+                            (xref--find-ignores-arguments ignores localdir)
+                            (if files
+                                (concat (shell-quote-argument "(")
+                                        " " find-name-arg " "
+                                        (mapconcat
+                                         #'shell-quote-argument
+                                         (split-string files)
+                                         (concat " -o " find-name-arg " "))
+                                        " "
+                                        (shell-quote-argument ")"))""))
+                    (format "rg --files | rg %s" localdir))))
+    (project--remote-file-names
+     (sort (split-string (shell-command-to-string command) "\0" t)
+           #'string<))))
+
+
 (use-package projectile :straight t
 	:config
 	(setq projectile-indexing-method 'alien
