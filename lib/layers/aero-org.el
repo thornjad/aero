93fd384fb51159a01c93d1e7d73fac50c2c70124
Jade Michael Thornton <jademichael@jmthornton.net>
2020-11-06 08:57:43 -0600

    auto eval thornlog functions

--- lib/layers/aero-org.el
+++ lib/layers/aero-org.el
@@ -30,6 +30,57 @@
   ;; org tries to take this binding back, so wrest control back once more
   (define-key org-mode-map (kbd "M-h") #'windmove-left)
 
-  (use-package toc-org :straight t))
+  (use-package toc-org :straight t)
+
+  (aero-mode-leader-def
+    :keymaps 'org-mode-map
+    "h" 'org-hide-block-all)
+  )
+
+(defvar aero/org-eval-safe-list
+  '(expand-file-name "~/doc/thornlog/")
+  "Directories which will have their contents evaluated without prompting.")
+
+(defun aero/safe-org-file-p (file)
+  "Determine if given filename is in a safe org path.
+Safe org paths are determined by `aero/org-eval-safe-list'."
+  (let ((file-path (file-name-directory file)))
+    (seq-some (lambda (x) (string= x file-path)) aero/org-eval-safe-list)))
+
+(defun aero/org-eval-startblock ()
+  "Evaluate the content of a code-block named 'aero/startblock' in the current
+  org-document, if present.
+
+  Emacs would usually prompt for permission as a safety precaution, but if the
+  buffer is associated with a filename matching any of the patterns inside the
+  list aero/org-eval-safe-list we just allow it. "
+  (aero/org-eval-named-block "aero/startblock"))
+
+(defun aero/org-eval-saveblock ()
+  "Evaluate the content of a code-block named 'aero/saveblock' in the current
+  org-document, if present.
+
+  Emacs would usually prompt for permission as a safety precaution, but if the
+  buffer is associated with a filename matching any of the patterns inside the
+  list aero/org-eval-safe-list we just allow it."
+  (aero/org-eval-named-block "aero/saveblock"))
+
+(defun aero/org-eval-named-block (name)
+  "Execute the named block, if it exists, from within the current file."
+  (save-excursion
+    (org-save-outline-visibility t
+      (when (and (aero/safe-org-file-p (buffer-file-name))
+                 (member name (org-babel-src-block-names)))
+        (progn
+          (setq-local org-confirm-babel-evaluate nil)
+          (org-babel-goto-named-src-block name)
+          (org-babel-execute-src-block))))))
+(add-hook 'org-mode-hook 'aero/org-eval-startblock)
+
+;; evaluation the save-block on save
+(defun aero/org-mode-before-save-hook-eval ()
+  (when (eq major-mode 'org-mode)
+    (aero/org-eval-saveblock)))
+(add-hook 'before-save-hook #'aero/org-mode-before-save-hook-eval)
 
 (provide 'aero-org)
