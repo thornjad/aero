7679dc16d492e6c21943906a19d7b244271a32b1
Jade Michael Thornton <jademichael@jmthornton.net>
2020-11-25 11:23:16 -0600

    move thornlog to zfs

--- lib/core/aero-lib.el
+++ lib/core/aero-lib.el
@@ -247,20 +247,24 @@ emacs with sigusr2"
   (interactive)
   (toggle-debug-on-quit))
 
+(defvar aero/thornlog-path
+  (expand-file-name "~/store/doc/thornlog/")
+  "Location of the thornlog on this filesystem.")
+
 (defun aero/thornlog-dir ()
   "Personal persistent log."
   (interactive)
-  (deer (expand-file-name "~/doc/thornlog/")))
+  (deer aero/thornlog-path))
 
 (defun aero/thornlog-log ()
   "Personal persistent log."
   (interactive)
-  (find-file (expand-file-name "~/doc/thornlog/log.org")))
+  (find-file (expand-file-name "log.org" aero/thornlog-path)))
 
 (defun aero/thornlog-todo ()
   "Personal persistent log todo."
   (interactive)
-  (find-file (expand-file-name "~/doc/thornlog/todo.org")))
+  (find-file (expand-file-name "todo.org" aero/thornlog-path)))
 
 (defun aero/eshell-taskwarrior ()
   "Open a specialized eshell buffer for taskwarrior."
