f39ef197af7374ec7b46619bcae26e37ad80c1c8
Jade Michael Thornton <jademichael@jmthornton.net>
2021-02-25 14:48:48 -0600

    remove broken reload-package function
    its been broken but i haven't felt the need for it, so gone it goes

--- lib/core/aero-lib.el
+++ lib/core/aero-lib.el
@@ -1106,48 +1106,4 @@ If called with prefix argument, or with nothing under point, prompt for tag."
   (interactive)
   (insert "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent libero orci, auctor sed, faucibus vestibulum, gravida vitae, arcu. Nunc posuere. Suspendisse potenti. Praesent in arcu ac nisl ultricies ultricies. Fusce eros. Sed pulvinar vehicula ante. Maecenas urna dolor, egestas vel, tristique et, porta eu, leo. Curabitur vitae sem eget arcu laoreet vulputate. Cras orci neque, faucibus et, rhoncus ac, venenatis ac, magna. Aenean eu lacus. Aliquam luctus facilisis augue. Nullam fringilla consectetuer sapien. Aenean neque augue, bibendum a, feugiat id, lobortis vel, nunc. Suspendisse in nibh quis erat condimentum pretium. Vestibulum tempor odio et leo. Sed sodales vestibulum justo. Cras convallis pellentesque augue. In eu magna. In pede turpis, feugiat pulvinar, sodales eget, bibendum consectetuer, magna. Pellentesque vitae augue."))
 
-(defun aero/reload-package (package &optional allp)
-  "Reload PACKAGE's features.
-If ALLP is non-nil (interactively, with prefix), load all of its
-features; otherwise only load ones that were already loaded.
-
-This is useful to reload a package after upgrading it.  Since a
-package may provide multiple features, to reload it properly
-would require either restarting Emacs or manually unloading and
-reloading each loaded feature.  This automates that process.
-
-Note that this unloads all of the package's symbols before
-reloading.  Any data stored in those symbols will be lost, so if
-the package would normally save that data, e.g. when a mode is
-deactivated or when Emacs exits, the user should do so before
-using this command."
-  (interactive
-   (list (intern (completing-read "Package: "
-                                  (mapcar #'car package-alist) nil t))
-         current-prefix-arg))
-  ;; This finds features in the currently installed version of PACKAGE, so if
-  ;; it provided other features in an older version, those are not unloaded.
-  (when (yes-or-no-p (format "Unload all of %s's symbols and reload its features? " package))
-    (let* ((package-name (symbol-name package))
-           (package-dir (file-name-directory
-                         (locate-file package-name load-path (get-load-suffixes))))
-           (package-files (directory-files package-dir 'full (rx ".el" eos)))
-           (package-features
-            (cl-loop for file in package-files
-                     when (with-temp-buffer
-                            (insert-file-contents file)
-                            (when (re-search-forward (rx bol "(provide" (1+ space)) nil t)
-                              (goto-char (match-beginning 0))
-                              (cadadr (read (current-buffer)))))
-                     collect it)))
-      (unless allp
-        (setf package-features (seq-intersection package-features features)))
-      (dolist (feature package-features)
-        (ignore-errors
-          ;; Ignore error in case it's not loaded.
-          (unload-feature feature 'force)))
-      (dolist (feature package-features)
-        (require feature))
-      (message "Reloaded: %s" (mapconcat #'symbol-name package-features " ")))))
-
 (provide 'aero-lib)
