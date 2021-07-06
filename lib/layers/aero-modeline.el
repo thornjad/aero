3a6692615cc801c9863c3e972c07ab82154a52f0
Jade Michael Thornton <jademichael@jmthornton.net>
2021-07-06 16:37:43 -0500

    decrease mode line height

--- lib/layers/aero-modeline.el
+++ lib/layers/aero-modeline.el
@@ -149,13 +149,13 @@
   "Displays a color-coded buffer modification indicator in the mode-line."
   (cond
    ((and buffer-read-only (buffer-file-name))  ;; read-only
-    (propertize "⛔" 'face `(:inherit aero/modeline-read-only :height 0.7)))
+    (propertize "⛔" 'face `(:inherit aero/modeline-read-only :height 0.6)))
    ((string-match-p "\\*.*\\*" (buffer-name))  ;; special buffer
-    (propertize "*" 'face `(:inherit aero/modeline-read-only :height 0.7)))
+    (propertize "*" 'face `(:inherit aero/modeline-read-only :height 0.6)))
    ((buffer-modified-p)  ;; modified
-    (propertize "●" 'face `(:inherit aero/modeline-modified :height 0.7)))
+    (propertize "●" 'face `(:inherit aero/modeline-modified :height 0.6)))
    (t  ;; not modified
-    (propertize "○" 'face `(:inherit aero/modeline-not-modified :height 0.7)))))
+    (propertize "○" 'face `(:inherit aero/modeline-not-modified :height 0.6)))))
 
 (defun aero/modeline-segment-remote ()
   "Displays a symbol if buffer is remote"
