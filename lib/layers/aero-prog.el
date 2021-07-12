cd37b8d97db95199f8835d0e2084ec310706d1be
Jade Michael Thornton <jademichael@jmthornton.net>
2021-07-12 11:08:13 -0500

    reduce company-box customization
    trust the defaults

--- lib/layers/aero-prog.el
+++ lib/layers/aero-prog.el
@@ -26,6 +26,7 @@
 	(setq company-idle-delay 0.2
         company-selection-wrap-around t
         company-minimum-prefix-length 2
+        company-dabbrev-downcase nil
         company-tooltip-limit 14
         company-require-match nil
         company-show-numbers t
@@ -51,42 +52,6 @@
   (setq company-box-show-single-candidate t
         company-box-backends-colors nil
         company-box-max-candidates 50
-        company-box-icons-alist 'company-box-icons-all-the-icons
-        ;; company-dabbrev-ignore-case t
-        company-dabbrev-downcase nil
-        company-box-icons-all-the-icons
-        (let ((all-the-icons-scale-factor 0.8))
-          `((Unknown . ,(all-the-icons-material "find_in_page" :face 'all-the-icons-purple))
-            (Text . ,(all-the-icons-material "text_fields" :face 'all-the-icons-green))
-            (Method . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (Function . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (Constructor . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (Field . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (Variable . ,(all-the-icons-material "adjust" :face 'all-the-icons-blue))
-            (Class . ,(all-the-icons-material "class" :face 'all-the-icons-red))
-            (Interface . ,(all-the-icons-material "settings_input_component" :face 'all-the-icons-red))
-            (Module . ,(all-the-icons-material "view_module" :face 'all-the-icons-red))
-            (Property . ,(all-the-icons-material "settings" :face 'all-the-icons-red))
-            (Unit . ,(all-the-icons-material "straighten" :face 'all-the-icons-red))
-            (Value . ,(all-the-icons-material "filter_1" :face 'all-the-icons-red))
-            (Enum . ,(all-the-icons-material "plus_one" :face 'all-the-icons-red))
-            (Keyword . ,(all-the-icons-material "filter_center_focus" :face 'all-the-icons-red))
-            (Snippet . ,(all-the-icons-material "short_text" :face 'all-the-icons-red))
-            (Color . ,(all-the-icons-material "color_lens" :face 'all-the-icons-red))
-            (File . ,(all-the-icons-material "insert_drive_file" :face 'all-the-icons-red))
-            (Reference . ,(all-the-icons-material "collections_bookmark" :face 'all-the-icons-red))
-            (Folder . ,(all-the-icons-material "folder" :face 'all-the-icons-red))
-            (EnumMember . ,(all-the-icons-material "people" :face 'all-the-icons-red))
-            (Constant . ,(all-the-icons-material "pause_circle_filled" :face 'all-the-icons-red))
-            (Struct . ,(all-the-icons-material "streetview" :face 'all-the-icons-red))
-            (Event . ,(all-the-icons-material "event" :face 'all-the-icons-red))
-            (Operator . ,(all-the-icons-material "control_point" :face 'all-the-icons-red))
-            (TypeParameter . ,(all-the-icons-material "class" :face 'all-the-icons-red))
-            (Template . ,(all-the-icons-material "short_text" :face 'all-the-icons-green))
-            (ElispFunction . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (ElispVariable . ,(all-the-icons-material "check_circle" :face 'all-the-icons-blue))
-            (ElispFeature . ,(all-the-icons-material "stars" :face 'all-the-icons-orange))
-            (ElispFace . ,(all-the-icons-material "format_paint" :face 'all-the-icons-pink))))
         x-gtk-resize-child-frames 'resize-mode)
 
   ;; disable tab bar in company-box child frames. Will probably be PR'd upstream eventually
