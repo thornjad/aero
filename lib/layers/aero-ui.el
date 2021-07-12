c21d619f7788fc8e584f2bacf98bf632cc06e46e
Jade Michael Thornton <jademichael@jmthornton.net>
2021-07-12 07:57:41 -0500

    use hl-line

--- lib/layers/aero-ui.el
+++ lib/layers/aero-ui.el
@@ -94,6 +94,7 @@
 (pixel-scroll-mode 1)
 (global-display-fill-column-indicator-mode 1)
 (global-visual-line-mode +1)
+(global-hl-line-mode +1)
 
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
