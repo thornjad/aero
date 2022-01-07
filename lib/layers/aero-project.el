352cebd3b65bca1b094b524714e703bfb3a1607c
Jade Michael Thornton <jademichael@jmthornton.net>
2022-01-07 12:52:30 -0600

    use better future-proofing

--- lib/layers/aero-project.el
+++ lib/layers/aero-project.el
@@ -23,8 +23,8 @@
 	:config
 	(setq projectile-indexing-method 'alien
 				projectile-enable-caching t
-				projectile-mode-line nil
-        projectile-globally-ignored-directories '(".idea" ".vscode" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" ".ccls-cache" ".cache" ".clangd" "node_modules"))
+				projectile-mode-line nil)
+  (add-to-list 'projectile-globally-ignored-directories "node_modules")
   (projectile-mode 1))
 
 (use-package counsel-projectile 
