1b31df99dcd42c2cc7145aa6b6b8a49b8584b6ba
Jade Michael Thornton <jademichael@jmthornton.net>
2022-02-18 13:23:37 -0600

    histogram thing

--- lib/layers/aero-heap.el
+++ lib/layers/aero-heap.el
@@ -83,6 +83,38 @@
   :commands (asm-blox)
   :init (aero-leader-def "aga" 'asm-blox))
 
+;; other crap
+
+;;; http://blogs.fluidinfo.com/terry/2011/11/10/emacs-buffer-mode-histogram/
+(defun buffer-mode-histogram ()
+  "Display a histogram of emacs buffer modes."
+  (interactive)
+  (let* ((totals ())
+         (buffers (buffer-list()))
+         (total-buffers (length buffers))
+         (ht (make-hash-table :test 'equal)))
+    (save-excursion
+      (dolist (buffer buffers)
+        (set-buffer buffer)
+        (let
+            ((mode-name (symbol-name major-mode)))
+          (puthash mode-name (1+ (gethash mode-name ht 0)) ht))))
+    (maphash (lambda (key value)
+               (setq totals (cons (list key value) totals)))
+             ht)
+    (setq totals (sort totals (lambda (x y) (> (cadr x) (cadr y)))))
+    (with-output-to-temp-buffer "Buffer mode histogram"
+      (princ (format "%d buffers open, in %d distinct modes\n\n"
+                     total-buffers (length totals)))
+      (dolist (item totals)
+        (let
+            ((key (car item))
+             (count (cadr item)))
+          (if (equal (substring key -5) "-mode")
+              (setq key (substring key 0 -5)))
+          (princ (format "%2d %20s %s\n" count key
+                         (make-string count ?+))))))))
+
 (use-package md4rd :straight t
   :after (general evil)
   :commands (md4rd)
