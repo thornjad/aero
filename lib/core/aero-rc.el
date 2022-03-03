6c713fa86f808367443a62d63499d404369c273d
Jade Michael Thornton <jademichael@jmthornton.net>
2022-03-03 10:25:27 -0600

    just a bit of scroll margin

--- lib/core/aero-rc.el
+++ lib/core/aero-rc.el
@@ -67,7 +67,7 @@
  ;; during large-scale scrolling commands. If kept over 100, the window is
  ;; never automatically recentered.
  scroll-conservatively 101
- scroll-margin 0
+ scroll-margin 3
  scroll-preserve-screen-position t
  mouse-wheel-scroll-amount '(3 ((shift) . 1))
  pixel-resolution-fine-flag 1
