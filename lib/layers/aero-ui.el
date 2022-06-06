f8e1e6dde86ff03060c8f6775fb22f1d56b8ba46
Jade Michael Thornton <jademichael@jmthornton.net>
2022-06-06 10:47:46 -0500

    remove duplicate ui change calls
    these are already done in early-init, and are slow

--- lib/layers/aero-ui.el
+++ lib/layers/aero-ui.el
@@ -88,10 +88,6 @@
 (global-display-fill-column-indicator-mode 1)
 (global-hl-line-mode +1)
 
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
 (use-package good-scroll
   :straight (:host github :repo "io12/good-scroll.el")
   :functions (good-scroll-mode)
