597b385edb0eea758206697d375badf1c818ea2b
Jade Michael Thornton <jademichael@jmthornton.net>
2022-06-15 09:30:53 -0500

    fix modeline squiggly

--- lib/layers/aero-modeline.el
+++ lib/layers/aero-modeline.el
@@ -169,7 +169,7 @@ respectively."
    ((string-match-p "\\*.*\\*" (buffer-name))  ;; special buffer
     (propertize "" 'face `(:inherit aero/modeline-read-only :height 0.6)))
    ((buffer-modified-p)  ;; modified
-    (propertize "" 'face `(:inherit aero/modeline-modified :height 0.6)))
+    (propertize "⏺" 'face `(:inherit aero/modeline-modified :height 0.6)))
    (t  ;; not modified
     (propertize "" 'face `(:inherit aero/modeline-not-modified :height 0.6)))))
 
