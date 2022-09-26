76581724abdd567bc4e15375869cfe1bb668f433
Jade Michael Thornton <jademichael@jmthornton.net>
2022-09-26 11:33:49 -0500

    linux deps update

--- bin/build/linux-requirements.zsh
+++ bin/build/linux-requirements.zsh
@@ -9,11 +9,15 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 sudo apt update
 
 libs=(
+  autoconf
   build-essential
   git
+  gnutls-bin
   libgccjit0
+  libgccjit-12-dev
   libgif-dev
   libgnutls30
+  libgtk-3-dev
   libgtk-4-dev
   libjansson-dev
   libjpeg-dev
@@ -22,5 +26,6 @@ libs=(
   libtiff5-dev
   libxml2-dev
   libxpm-dev
+  texinfo
 )
-sudo apt install ${libs[@]}
+sudo apt install -y ${libs[@]}
