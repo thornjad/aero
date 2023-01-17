9a751f301ffdb67a7e4bc8851d35d672ee6eed4c
Jade Michael Thornton <jademichael@jmthornton.net>
2023-01-17 12:07:21 -0500

    clean up eshell config

--- lib/layers/aero-shell.el
+++ lib/layers/aero-shell.el
@@ -65,16 +65,22 @@
    eshell-visual-commands '("vi" "screen" "top" "less" "more" "lynx"
                             "ncftp" "pine" "tin" "trn" "elm" "vim"
                             "nmtui" "alsamixer" "htop" "el" "elinks"
-                            "ssh" "nethack" "dtop" "dstat")
+                            "ssh" "nethack" "dtop" "dstat" "docker-compose")
    eshell-visual-subcommands '(("git" "log" "diff" "show"))
+   eshell-cmpl-cycle-completions nil
+   eshell-buffer-maximum-lines 12000 ; auto truncate after 12k lines
+   eshell-history-size 500 ; history size
+   eshell-buffer-shorthand t ; buffer shorthand -> echo foo > #'buffer
+   eshell-plain-echo-behavior t ; treat 'echo' like shell echo
    completion-ignore-case t)
-  (setenv "PAGER" "bat")
 
+  ;; Try to load in PATH
   (let ((default-directory (expand-file-name "~")))
     (setq eshell-path-env (getenv "PATH")))
 
   ;; doesn't handle less too well
-  (setenv "PAGER" "cat")
+  ;; (setenv "PAGER" "cat")
+  (setenv "PAGER" "bat")
   (setenv "TERM" "xterm-256color")
 
   (add-hook
@@ -84,47 +90,11 @@
                  scroll-margin 0)))
 
   (defalias 'eshell/emacs 'find-file)
-  (defun eshell/clear ()
-    (recenter 0))
-  (defun eshell/magit ()
-    "Open magit-status in the current directory."
-    (interactive)
-    (magit-status-setup-buffer default-directory))
 
-  (dolist (x '(('e . (lambda (pattern)
-                       (if (stringp pattern)
-                           (find-file pattern)
-                         (mapc #'find-file
-                               (mapcar #'expand-file-name pattern)))))
-
-               ('ee . #'find-file-other-window)
-               ('eshell/la . #'eshell/ls)
-               ('eshell/g . #'eshell/git)))
-    (eval `(defalias ,(car x) ,(cdr x))))
-
-  (defun eshell/ec (pattern)
+  (defun eshell/e (pattern)
     (if (stringp pattern)
         (find-file pattern)
       (mapc #'find-file (mapcar #'expand-file-name pattern))))
-  (defalias 'e 'eshell/ec)
-  (defalias 'ee 'find-file-other-window)
-
-  (defun eshell/sudoec (file)
-    (interactive)
-    (if (file-remote-p file)
-        ;; TODO
-        (message "can't sudoec a remote file yet")
-      (find-file (concat "/sudo::" (expand-file-name file)))))
-
-  (defun eshell/icat (&rest args)
-    "Display image(s)."
-    (let ((elems (flatten-list args)))
-      (while elems
-        (eshell-printn
-         (propertize " "
-                     'display (create-image (expand-file-name (car elems)))))
-        (setq elems (cdr elems))))
-    nil)
 
   ;; So the history vars are defined
   (require 'em-hist)
@@ -138,27 +108,11 @@
        (require 'em-prompt)
        (require 'em-term)
 
-       (setq eshell-cmpl-cycle-completions nil
-             ;; auto truncate after 12k lines
-             eshell-buffer-maximum-lines 12000
-             ;; history size
-             eshell-history-size 500
-             ;; buffer shorthand -> echo foo > #'buffer
-             eshell-buffer-shorthand t
-             ;; treat 'echo' like shell echo
-             eshell-plain-echo-behavior t
-             eshell-ls-initial-args "-lah")
-
-       ;; Visual commands
-       (setq eshell-visual-commands '("vi" "screen" "top" "less" "more" "lynx"
-                                      "ncftp" "pine" "tin" "trn" "elm" "vim"
-                                      "nmtui" "alsamixer" "htop" "el" "elinks"
-                                      "ssh" "nethack" "dtop" "dstat"))
-       (setq eshell-visual-subcommands '(("git" "log" "diff" "show")
-                                         ("vagrant" "ssh"))))))
+       (setq
+        ))))
 
 
-;;; vterm
+;; vterm
 
 ;; NOTE: vterm requires libvterm-dev, which may not be installed. See
 ;; https://github.com/akermu/emacs-libvterm for full install instructions. Also requires shell-side
