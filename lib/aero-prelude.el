f63734c1d8e98139da73bfcde9ad4631d2e9b467
Jade Michael Thornton <jademichael@jmthornton.net>
2023-05-05 10:47:18 -0400

    longer delay

--- lib/aero-prelude.el
+++ lib/aero-prelude.el
@@ -955,10 +955,9 @@ Useful for when undo-tree inevitably fucks up the file and it can't be read."
 (package! copilot (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
   :after company-box
   :hook (prog-mode . copilot-mode)
-  :commands (copilot-mode)
-  :custom (copilot-idle-delay 0.5)
   :config
   (define-key copilot-completion-map (kbd "C-<tab>") 'copilot-accept-completion)
+  :custom (copilot-idle-delay 0.7)
   (define-key copilot-completion-map (kbd "C-TAB") 'copilot-accept-completion))
 
 ;; Mark E′ violations
