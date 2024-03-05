e4beb2a6d64978ad547d64229e5668e7742bbe41
Jade Michael Thornton <jademichael@jmthornton.net>
2024-03-05 15:56:00 -0500

    add meeting skipped state

--- lib/layers/aero-org.el
+++ lib/layers/aero-org.el
@@ -108,8 +108,8 @@
 
   (org-todo-keywords
    '((sequence "TODO(t)" "WAITING(w!)" "BLOCKED(b!)" "|" "DONE(d!)" "REMOVED(k)")
-     (sequence "TICKET(T)" "PR(p!)" "|" "DONE(d!)" "CLOSED(k)")
-     (sequence "MEETING(m)" "|" "DONE(d)" "CANCELLED(k)")))
+     (sequence "TICKET(T)" "PR(p!)" "|" "DONE(d!)" "CLOSED(x)")
+     (sequence "MEETING(m)" "|" "DONE(d)" "CANCELLED(c)" "SKIPPED(s)")))
 
   (org-use-fast-todo-selection 'expert) ; don't fuck up the window layout
   (org-default-notes-file (expand-file-name "notes.org" aero/thornlog-path))
