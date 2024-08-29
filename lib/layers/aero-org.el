1066ada3c647094c50132ea6bb63c4ccbb0b9e22
Jade Michael Thornton <jademichael@jmthornton.net>
2024-08-29 11:12:26 -0500

    tweak agenda yet again

--- lib/layers/aero-org.el
+++ lib/layers/aero-org.el
@@ -418,7 +418,7 @@ This function makes sure that dates are aligned for easy reading."
      (:name "Outstanding meetings" :and (:scheduled past :tag "meeting"))
      (:time-grid t)
      (:name "Priority A" :and (:priority "A" :not (:todo "PR")))
-     (:name "Reviews" :tag "review")
+     (:name "Reviews to do" :and (:tag "review" :todo "REVIEW"))
      (:name "Support" :tag "support")
      (:name "Tickets and PRs" :todo "PR" :todo "TICKET")
      (:deadline past)
@@ -426,7 +426,7 @@ This function makes sure that dates are aligned for easy reading."
      (:name "Priority B" :and (:priority "B" :not (:todo "WAITING" :todo "BLOCKED")))
      (:name "Past scheduled" :and (:scheduled past :not (:todo "WAITING" :todo "BLOCKED")))
      (:name "Prioritized" :and (:priority<= "B" :not (:todo "WAITING" :todo "BLOCKED")))
-     (:name "Waiting/Blocked" :todo "WAITING" :todo "BLOCKED")
+     (:name "Waiting/blocked" :todo "WAITING" :todo "BLOCKED")
      (:name "Due soon" :deadline future)))
 
   ;; add space between dates by adding space after the final group
