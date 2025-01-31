b8965562e6ad61098c6f78003065fa257e197b73
Jade Michael Thornton <jademichael@jmthornton.net>
2025-01-30 22:48:34 -0600

    cleanup

--- lib/localpackages/aero-breadcrumb.el
+++ lib/localpackages/aero-breadcrumb.el
@@ -12,9 +12,6 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 ;;; Commentary:
 ;;
 ;;;; Usage:
